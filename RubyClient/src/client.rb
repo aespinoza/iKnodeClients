e9df90f4a9a8801841aa05ac2d6666612fed42cf
Alberto Avila <albertoavila@gmail.com>
2012-03-07 13:32:42 -0700

    client now works correctly

--- RubyClient/src/client.rb
+++ RubyClient/src/client.rb
@@ -4,6 +4,8 @@ def main()
   service_url = ""
   api_key = ""
   user_id = ""
+
+  #parse config file
   file = File.new("config", "r")
   line_count = 0
   while (line = file.gets)
@@ -26,6 +28,25 @@ def main()
   if service_url == "" or api_key == "" or user_id == ""
     abort "Incomplete config file, must contain service_url, api_key and user_id values" 
   end
+  
+  #validate ARGV
+  if ARGV.length == 0
+    abort "Usage: ApplicationName:MethodName [--param_name=para_value]..."
+  end
+
+  #prepare request
+  uri = URI(service_url)
+  body = "{\"userId\": \"" + user_id + "\", \"apiKey\": \"" + api_key + 
+    "\", \"command\": \""+ ARGV[0] + "\"}"
+  request = Net::HTTP::Post.new(uri.path, initheader = {"Content-Type" => 
+                                  "application/json"})
+  request.body = body
+  http = Net::HTTP.new(uri.host, uri.port) 
+  http.use_ssl = uri.scheme == 'https' 
+  response = http.start do |http|
+    http.request(request)
+  end
+  puts response.body
 end
 
 main
